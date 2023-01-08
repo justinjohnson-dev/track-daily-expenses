f5e08fad2e295e74cff381a702c0f80dc0eab985
Justin Johnson <jjustin634@gmail.com>
2023-01-08 15:37:21 -0600

    algorithm to find reports by selected date

---
+++ lib/find-reports-by-date.ts
@@ -0,0 +1,29 @@
+// for different types of reports
+// lookup for finding date key
+const REPORT_TYPE = {
+  expense: 'expenseDate',
+  income: 'incomeDate',
+};
+
+export default function formatToDatetimeAndFilterBySelectedMonth(
+  arrayOfReports,
+  filterDate,
+  reportType
+) {
+  const reportDate = REPORT_TYPE[reportType];
+  const reportsForCurrentMonth: string[] = [];
+
+  arrayOfReports.forEach((report, index) => {
+    const reportEntryDate = report[reportDate];
+    const dateCutoffValue = reportEntryDate.indexOf(' at '); // space between at to avoid 'at' in 'Saturday'
+    const stringDateToConvert = reportEntryDate.substring(
+      0,
+      dateCutoffValue - 1
+    );
+    const reportMonth = new Date(stringDateToConvert).getMonth() + 1;
+
+    if (filterDate === reportMonth) reportsForCurrentMonth.push(report);
+  });
+
+  return reportsForCurrentMonth;
+}
