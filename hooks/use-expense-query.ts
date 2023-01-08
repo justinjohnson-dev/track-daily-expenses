0f6a2d02aa0f53d4f3b7bf5c34a6b9e32486759c
Justin Johnson <jjustin634@gmail.com>
2023-01-08 15:36:54 -0600

    adding income query by month

--- hooks/use-expense-query.ts
+++ hooks/use-expense-query.ts
@@ -1,6 +1,6 @@
 import { useQuery } from 'react-query';
 
-export default function useExpenseQuery(month) {
+export default function useExpenseQuery(month: number) {
   return useQuery(
     ['expenses', month],
     () => fetch(`/api/expenses/${month}`).then((res) => res.json()),
