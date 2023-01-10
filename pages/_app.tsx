4db978d28c4c9cffd57db3a48511ea847c0c589f
Justin Johnson <jjustin634@gmail.com>
2023-01-10 13:27:07 -0600

    adding proper layout around all components/pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app';
 import queryClient from '../lib/query-client';
 import { QueryClientProvider } from 'react-query';
+import Layout from '../components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Layout>
+        {' '}
+        <Component {...pageProps} />
+      </Layout>
     </QueryClientProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,26 @@
-import Layout from '../components/layout';
 import ExpenseForm from '../components/expense/expenseForm';
 import IncomeForm from '../components/income/incomeForm';
 
 export default function Home() {
   return (
     <>
-      <Layout />
-      <main
+      <div
         style={{
-          alignItems: 'center',
-          minHeight: '100vh',
+          width: '90%',
+          margin: '25px auto 65px auto',
         }}
       >
-        <div
-          style={{
-            width: '90%',
-            margin: '25px auto 65px auto',
-          }}
-        >
-          <ExpenseForm />
-        </div>
+        <ExpenseForm />
+      </div>
 
-        <div
-          style={{
-            width: '90%',
-            margin: '50px auto',
-          }}
-        >
-          <IncomeForm />
-        </div>
-      </main>
+      <div
+        style={{
+          width: '90%',
+          margin: '50px auto',
+        }}
+      >
+        <IncomeForm />
+      </div>
     </>
   );
 }
