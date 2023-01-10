fd13aae9f5412779d8b17a75a0847cf87672e062
Justin Johnson <jjustin634@gmail.com>
2023-01-10 14:28:57 -0600

    adding loading spinner for requests

---
+++ components/circularLoadingBar.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export default function CircularIndeterminate() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        maxWidth: '10%',
+        margin: '0 auto',
+        padding: '1%',
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
