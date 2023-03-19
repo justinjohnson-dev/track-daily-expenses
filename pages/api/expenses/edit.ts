0325608708ddd8c7c943587446c47081a2399da5
Justin Johnson <jjustin634@gmail.com>
2023-03-18 22:24:47 -0500

    adding prisma queries to PUT and DELETE expense records

---
+++ pages/api/expenses/edit.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+/* eslint-disable */
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { StatusCodes } from 'http-status-codes';
+
+import { editUserExpense } from '../../../services/expense';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { method, body } = req;
+  if (method === 'PUT') {
+    const edittedExpense = await editUserExpense(body);
+
+    return res.status(StatusCodes.OK).send(edittedExpense);
+  }
+}
